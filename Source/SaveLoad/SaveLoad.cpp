083510185bbb2a8c98f4ce6fc1f437657ed5a7ae
poi001 <107660181+poi001@users.noreply.github.com>
2023-06-08 01:54:46 +0900

    Initial Push
    First Push

---
+++ Source/SaveLoad/SaveLoad.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SaveLoad.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SaveLoad, "SaveLoad" );
+ 
