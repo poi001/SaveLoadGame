377af71c75846d9d55ed632e3818080929905b4e
poi001 <107660181+poi001@users.noreply.github.com>
2023-07-03 17:05:13 +0900

    Delete

--- Source/SaveLoad/SaveLoad.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SaveLoad.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SaveLoad, "SaveLoad" );
- 
